ba689628e6f06ee860656810c868c2d6cbb359c4
kjkynd <kjkynd@missouri.edu>
2025-03-12 23:10:06 -0500

    Create StockDataVisualizer.py
    Initial check-in.  No code, just comments indicating necessary actions.

---
+++ StockDataVisualizer.py
@@ -0,0 +1,15 @@
+##INFO TECH 4430 - Software Engineering
+#Project 3 - Stock Data Visualization
+#Scrum Team 2 - Sam Herrmann (Scrum Master), Zoey Benedict, Karson Null, Ken Kreidell
+
+#Collect graph paramters
+
+#Call API to get data
+
+#Manipulate data
+
+#Create graph
+
+#Write HTML wrapper for graph
+
+#Launch HTML in default browser.
